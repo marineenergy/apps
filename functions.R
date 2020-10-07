f9f3687e13c3e33a1de732c490d2576c1339464b
Ben Best <ben@ecoquants.com>
2020-10-07 16:38:55 +0000

    loaded shp_* datasets

--- functions.R
+++ functions.R
@@ -67,28 +67,52 @@ titleshp2tbl <- function(title, shp){
   tbl
 }
 
+shp2tbl <- function(shp_path){
+  # title = "2012 - 2017 Outer Continental Shelf Oil and Gas Leasing Program"
+  # shp   = "GOM_CGM_2012_2017_PFP_Area.shp"
+  #shp_path <- shps$shp_path[1]
+  # which.max(map_int(shps$shp_path, function(x) nchar(basename(x) %>% fs::path_ext_remove())))
+  #shp_path <- shps$shp_path[325]
+
+  tbl_sfx <- basename(shp_path) %>% fs::path_ext_remove()
+  tbl     <- glue("shp_{tbl_sfx}") %>% str_sub(end=63)
+
+  tbl
+}
+
 shp2db <- function(shp, tbl, redo = F){
   # redo = F
   # shp  = "/share/data/marinecadastre.gov/Pacific Northwest Physiographic Habitat/V4_0_SGH_WA_OR_NCA_dir/cromsos/ShapefileData/V4_0_SGH_WA_OR_NCA/V4_0_SGH_WA_OR_NCA.shp"
   # tbl  = "Pacific Northwest Physiographic Habitat - V4_0_SGH_WA_OR_NCA.shp"
-  
+  # tbl = "shp_geology"; shp = dataset_shps4db %>% filter(tbl == !!tbl) %>% pull(shp)
+
   message(glue("LOAD: {tbl}"))
   
-  d_sf <- read_sf(shp)
-  
-  names(d_sf) <- dbSafeNames(names(d_sf))
-  
-  # d_tbl  <- "d_efh" # prefix: mc_ for MarineCadastre, ds_ for dataset?
-  # d_redo <- FALSE
-  
-  # project to geographic coordinate reference system if need be
-  if (st_crs(d_sf) != st_crs(4326)){
-    d_sf <- st_transform(d_sf, crs = 4326)
-  }
-  
   if (!tbl %in% dbListTables(con) | redo){
+    d_sf <- read_sf(shp)
+    
+    names(d_sf) <- dbSafeNames(names(d_sf))
+    
+    # d_tbl  <- "d_efh" # prefix: mc_ for MarineCadastre, ds_ for dataset?
+    # d_redo <- FALSE
+    
+    # project to geographic coordinate reference system if need be
+    if (is.na(st_crs(d_sf))){
+      message("  st_set_crs(4326)")
+      st_crs(d_sf) = 4326
+    }
+    if (st_crs(d_sf) != st_crs(4326)){
+      message("  st_transform(crs = 4326)")
+      d_sf <- st_transform(d_sf, crs = 4326)
+    }
+    
+    message("  st_write()")
     st_write(d_sf, con, tbl)
+    
+  } else {
+    message("  already loaded")
   }
+  
   if (tbl %in% dbListTables(con)){
     return(TRUE)
   } else {
