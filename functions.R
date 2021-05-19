2c6e12448664868d0cb82c7771f3892e081a13af
Ben Best <ben@ecoquants.com>
2021-05-19 20:11:20 +0000

    + update_tags() from gsheet w/ gs4_auth()

--- functions.R
+++ functions.R
@@ -63,7 +63,7 @@ con <<- dbConnect(
   user     = db_params$user,
   password = readLines(db_params$pwd_txt))
 
-# tbls <- dbListTables(con); tbls
+# tbls <- dbListTables(con) %>% sort(); tbls
 
 # helper functions
 drop_d <- function(d_tbl){
@@ -746,6 +746,35 @@ html_add <- function(content){
 
 html_out <- function(){
   html_tags
+}
+
+update_tags <- function(){
+  shelf(googlesheets4)
+  
+  # google sheet key from Google Console service account
+  #   https://console.cloud.google.com/iam-admin/serviceaccounts/details/111453668228815650069/keys?authuser=2&organizationId=651265387478&project=marineenergy4gargle
+  gs4_auth_json <- "/share/data/marineenergy4gargle.json" 
+  # tags tab in [data | marineenergy.app - Google Sheet](https://docs.google.com/spreadsheets/d/1MTlWQgBeV4eNbM2JXNXU3Y-_Y6QcOOfjWFyKWfdMIQM/edit#gid=662531985)
+  #   + shared sheet with: shares@marineenergy4gargle.iam.gserviceaccount.com
+  sheet_id  <- "1MTlWQgBeV4eNbM2JXNXU3Y-_Y6QcOOfjWFyKWfdMIQM"
+  sheet_tab <- "tags"
+  
+  # rename original tags
+  # DBI::dbSendQuery(con, "ALTER TABLE tags RENAME TO tags_0;")
+  # dbListTables(con) %>% sort()
+  
+  # googledrive
+  stopifnot(file.exists(gs4_auth_json))
+  gs4_auth(path = gs4_auth_json)
+  
+  # read tags from gsheet
+  tags <- read_sheet(sheet_id, sheet_tab)
+  
+  # write tags to db
+  dbWriteTable(con, "tags", tags, overwrite=T)
+  
+  dbSendStatement(con, "ALTER TABLE tags ALTER COLUMN tag_sql TYPE ltree USING text2ltree(tag_sql);")
+  
 }
 
 update_tethys_docs <- function(){
