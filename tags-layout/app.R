a07ee9a278fc2bb45cc74fc792f7325636e61872
Ben Best <ben@ecoquants.com>
2021-05-23 01:27:52 +0000

    + top nav menus hack; + svg logo

--- tags-layout/app.R
+++ tags-layout/app.R
@@ -1,11 +1,158 @@
 # libraries ----
 library(librarian)
-# shelf(r-lib/ellipsis, RinteRface/shinydashboardPlus, r-lib/vctrs)
 shelf(
   DT, googleAuthR, here, mapedit, shiny, shinydashboard, shinyjs, shinyWidgets, sf)
-library(shinydashboardPlus) # overwrites shinydashboard
+library(shinydashboardPlus) # overwrites shinydashboard functions
 
 source(here("functions.R"))
+
+# navbar ----
+dashboardHeader <- function(
+  ..., title = NULL, titleWidth = NULL, 
+  disable = FALSE, .list = NULL, leftUi = NULL,
+  controlbarIcon = shiny::icon("gears"), fixed = FALSE) {
+  
+  # handle right menu items
+  items <- c(list(...), .list)
+  lapply(items, shinydashboardPlus:::tagAssert, type = "li", class = "dropdown")
+  
+  # handle left menu items
+  if (!is.null(leftUi)) {
+    left_menu_items <- leftUi
+    # left_menu_items <- lapply(seq_along(leftUi), FUN = function(i) {
+    #   left_menu_item <- leftUi[[i]]
+    #   name <- left_menu_item$name
+    #   class <- left_menu_item$attribs$class
+    #   
+    #   # if the left menu item is not a li tag and does not have
+    #   # the dropdown class, create a wrapper to make it work
+    #   if (name != "li" || !is.null(class) || class != "dropdown") {
+    #     dropdownTag <- shiny::tags$li(class = "dropdown")
+    #     left_menu_item <- shiny::tagAppendChild(dropdownTag, left_menu_item)
+    #     # add some custom css to make it nicer
+    #     left_menu_item <- shiny::tagAppendAttributes(
+    #       left_menu_item,
+    #       style = "margin-top: 7.5px; margin-left: 5px; margin-right: 5px;"
+    #     )
+    #   } else {
+    #     left_menu_item
+    #   }
+    # })
+    # when left_menu is null, left_menu_items are also NULL 
+  } else {
+    left_menu_items <- leftUi
+  }
+  
+  titleWidth <- shiny::validateCssUnit(titleWidth)
+  
+  # Set up custom CSS for custom width.
+  custom_css <- NULL
+  if (!is.null(titleWidth)) {
+    # This CSS is derived from the header-related instances of '230px' (the
+    # default sidebar width) from inst/AdminLTE/AdminLTE.css. One change is that
+    # instead making changes to the global settings, we've put them in a media
+    # query (min-width: 768px), so that it won't override other media queries
+    # (like max-width: 767px) that work for narrower screens.
+    custom_css <- shiny::tags$head(
+      shiny::tags$style(
+        shiny::HTML(
+          gsub(
+            "_WIDTH_", 
+            titleWidth, 
+            fixed = TRUE, 
+            '@media (min-width: 768px) {
+              .main-header > .navbar {
+                margin-left: _WIDTH_;
+              }
+              .main-header .logo {
+                width: _WIDTH_;
+              }
+             }
+              '
+          )
+        )
+      )
+    )
+  }
+  
+  shiny::tags$header(
+    class = "main-header",
+    custom_css,
+    style = if (disable) "display: none;",
+    # only hide on small screen devices when title is NULL
+    shiny::tags$span(class = if (is.null(title)) "logo hidden-xs" else "logo", title),
+    shiny::tags$nav(
+      class = paste0("navbar navbar-", if (fixed) "fixed" else "static", "-top"), 
+      role = "navigation",
+      # Embed hidden icon so that we get the font-awesome dependency
+      shiny::tags$span(shiny::icon("bars"), style = "display:none;"),
+      # Sidebar toggle button
+      shiny::tags$a(
+        href = "#", 
+        class = "sidebar-toggle", 
+        `data-toggle` = "offcanvas",
+        role = "button",
+        shiny::tags$span(class = "sr-only", "Toggle navigation")
+      ),
+      # left menu
+      shiny::tags$div(
+        class = "navbar-custom-menu",
+        style = "float: left; margin-left: 10px;",
+        shiny::tags$ul(
+          class = "nav navbar-nav",
+          left_menu_items
+        )
+      ),
+      # right menu
+      shiny::tags$div(
+        class = "navbar-custom-menu",
+        shiny::tags$ul(
+          class = "nav navbar-nav",
+          items,
+          # right sidebar
+          shiny::tags$li(
+            shiny::tags$a(
+              href = "#", 
+              `data-toggle` = "control-sidebar", 
+              controlbarIcon
+            )
+          )
+        )
+      )
+    )
+  )
+}
+
+# trick: [How to Show Tabpanels in bs4navbar() ? · Issue #108 · RinteRface/bs4Dash](https://github.com/RinteRface/bs4Dash/issues/108)
+
+navbarTab <- function(tabName, ..., icon = NULL) {
+  tags$li(
+    class = "nav-item",
+    tags$a(
+      class = "nav-link",
+      id = paste0("tab-", tabName),
+      href = paste0("#shiny-tab-", tabName),
+      `data-toggle` = "tab",
+      `data-value` = tabName,
+      icon,
+      tags$p(...)))
+}
+
+
+navbarMenu <- function(..., id = NULL) {
+  if (is.null(id))
+    id <- paste0("tabs_", round(stats::runif(1, min = 0, max = 1e9)))
+  
+  tags$ul(
+    class = "navbar-nav dropdown sidebar-menu", 
+    role = "menu",
+    ...,
+    div(
+      id = id,
+      class = "sidebarMenuSelectedTabItem",
+      `data-value` = "null"))
+}
+
 
 # tag_choices ----
 df_tags <- tbl(con, "tags") %>% 
@@ -35,20 +182,9 @@ for (cat in unique(df_tags$category)){ # (cat = df_tags$category[1])
 # * [Client ID for Web application – APIs & Services – iea-uploader – Google API Console](https://console.developers.google.com/apis/credentials/oauthclient/596429062120-lsgv9f8pbas11o7ub9b9cm0earf1dpu5.apps.googleusercontent.com?authuser=3&project=iea-uploader)
 options(googleAuthR.webapp.client_id = "596429062120-lsgv9f8pbas11o7ub9b9cm0earf1dpu5.apps.googleusercontent.com")
 
-googleSignInUI_btn_signin <- function(id, logout_name = "Sign Out", logout_class = "btn-danger"){
-  ns <- shiny::NS(id)
-  
-  shiny::tagList(
-    div(id=ns("signin"), class="g-signin2", "data-onsuccess"="onSignIn"))
-}
-
-googleSignInUI_btn_signout <- function(id, logout_name = "Sign Out", logout_class = "btn-danger"){
-  ns <- shiny::NS(id)
-  
-  shiny::tagList(
-    tags$button(id = ns("signout"), logout_name, onclick = "signOut();", class = logout_class))
-}
-
+# googleSignInUI() original definition:
+#   https://github.com/MarkEdmondson1234/googleAuthR/blob/73221f3cf5a561cbb17fe04943ce85e2130204ff/R/shiny-js-signin.R#L16-L37
+#   broke up into 3 bits for placement: *_head, *_btn_signin, *_btn_signout
 googleSignInUI_head <- function(id, logout_name = "Sign Out", logout_class = "btn-danger"){
   
   ns <- shiny::NS(id)
@@ -66,6 +202,21 @@ googleSignInUI_head <- function(id, logout_name = "Sign Out", logout_class = "bt
       ns("g_id"), ns("g_name"), ns("g_image"), ns("g_email"), ns("signed_in")))
 }
 
+googleSignInUI_btn_signin <- function(id, logout_name = "Sign Out", logout_class = "btn-danger"){
+  ns <- shiny::NS(id)
+  
+  shiny::tagList(
+    div(id=ns("signin"), class="g-signin2", "data-onsuccess"="onSignIn"))
+}
+
+googleSignInUI_btn_signout <- function(id, logout_name = "Sign Out", logout_class = "btn-danger"){
+  ns <- shiny::NS(id)
+  
+  shiny::tagList(
+    tags$button(id = ns("signout"), logout_name, onclick = "signOut();", class = logout_class))
+}
+
+
 ## map_edit ----
 
 map_edit <- leaflet(
@@ -77,41 +228,59 @@ map_edit <- leaflet(
 
 # ui ----
 ui <- dashboardPage(
+  
+  #* header ----
   dashboardHeader(
-    title = "MarineEnergy.app",
+    title = HTML("<a class='navbar-brand' href='#'><img alt='Brand' src='./images/logo-horizontal-square.svg' width='40px'></a>MarineEnergy.app"),
     titleWidth = 310,
+    leftUi = navbarMenu(
+      navbarTab(tabName = "Tab1", "Tab 1"),
+      navbarTab(tabName = "Tab2", "Tab 2")),
     tags$li(
-      class = "dropdown",
-      tags$li(
-        googleSignInUI_btn_signin("login"), class = "dropdown"),
-      userOutput("user"))),
-    
+      googleSignInUI_btn_signin("login"), class = "dropdown"),
+    userOutput("user")),
+
+  #* sidebar ----
   dashboardSidebar(
     width = 310,
     googleSignInUI_head("login"),
-    wellPanel(
-      h4("Interactions"),
-      selectInput(
-        "sel_ixn_tags", "Tags", tag_choices, multiple = T),
-      uiOutput("ixn_btns")),
-    wellPanel(
-      h4("Location"),
-      div(
-        class="shiny-input-container",
-        leafletOutput("map_side", height = 200)),
-      actionButton(
-        "btn_mod_map", "Add", icon = icon("plus"), width = "270px"))),
+    sidebarMenu(
+      menuItem(
+        "Configure", 
+        tabName = "configure",icon = icon("gear"),
+        startExpanded = T,
+        wellPanel(
+          h4("Interactions"),
+          selectInput(
+            "sel_ixn_tags", "Tags", tag_choices, multiple = T),
+          uiOutput("ixn_btns")),
+        wellPanel(
+          h4("Location"),
+          div(
+            class="shiny-input-container",
+            leafletOutput("map_side", height = 200)),
+          actionButton(
+            "btn_mod_map", "Add", icon = icon("plus"), width = "263px"))))),
   
+  #* body ----
   dashboardBody(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")),
-    # boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 250)),
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50)
-      )))
+    
+    tabItems(
+      tabItem(
+        tabName = "Tab1",
+        "Tab 1"),
+      tabItem(
+        tabName = "Tab2",
+        "Tab 2",
+        fluidRow(
+          # boxes need to be put in a row (or column)
+          box(plotOutput("plot1", height = 250)),
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )))))
 )
 
 # server ----
@@ -185,7 +354,7 @@ server <- function(input, output, session) {
     if (length(values$ixns) == 0)
       return(
         actionButton(
-          "btn_add_ixn", "Add", icon=icon("plus"), width="270px"))
+          "btn_add_ixn", "Add", icon=icon("plus"), width="263px"))
     
     tagList(
       actionButton(
