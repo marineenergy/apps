c09947d9499dfd4fd809f3c44e0ced509b14bc4e
Ben Best <ben@ecoquants.com>
2022-02-21 23:02:09 +0000

    #message()

--- report-v2/global.R
+++ report-v2/global.R
@@ -11,8 +11,8 @@ librarian::shelf(
   sf, shinydashboard, RinteRface/shinydashboardPlus, shiny, shinycssloaders, shinyjs, shinyWidgets)
 renderDataTable <- DT::renderDataTable
 # devtools::session_info() # confirm versions, esp shinydashboardPlus
-library(reactlog)
-reactlog_enable()
+# library(reactlog)
+# reactlog_enable()
 # run a shiny app
 # runApp("/share/github/apps_dev/report-v2")
 # once app has closed, display reactlog from shiny
